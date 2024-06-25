2ba9f9b3ba4e104c3575c24de77f84a1e1ea4a54
Antaripa Saha <antaripasaha2000@gmail.com>
2024-06-25 13:18:46 +0530

    added simple animation

--- reminders/static/script.js
+++ reminders/static/script.js
@@ -1,15 +1,10 @@
-function openEditForm(taskId, title, category, dueDate) {
-    document.getElementById('edit-task-title').value = title;
-    document.getElementById('edit-task-category').value = category;
-    document.getElementById('edit-task-date').value = dueDate;
-    document.getElementById('edit-task-form').action = `/edit_task/${taskId}/`;
-    document.getElementById('edit-task-modal').classList.remove('hidden');
-}
 
 document.addEventListener('DOMContentLoaded', function() {
     const addTaskButton = document.getElementById('add-task-button');
+    const addTaskModal = document.getElementById('add-task-modal');
+    const editTaskModal = document.getElementById('edit-task-modal');
     const addTaskForm = document.getElementById('add-task-form');
-    const categorySelect = document.getElementById('id_category');
+    const categorySelect = document.getElementById('task_category');
     const taskDateInput = document.getElementById('task_date');
 
     function updateAddTaskForm() {
@@ -34,16 +29,20 @@ document.addEventListener('DOMContentLoaded', function() {
         categorySelect.value = category;
     }
 
-    updateAddTaskForm();
-
-    // Update to handle toggling of 'hidden' class and updating form
     addTaskButton.addEventListener('click', function() {
-        addTaskForm.classList.toggle('hidden');
+        addTaskModal.style.display = 'block';
         updateAddTaskForm();
-        
-        // Scroll to the bottom of the card when the form is shown
-        if (!addTaskForm.classList.contains('hidden')) {
-            addTaskForm.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    });
+
+    addTaskModal.addEventListener('click', function(e) {
+        if (e.target === addTaskModal) {
+            addTaskModal.style.display = 'none';
+        }
+    });
+
+    editTaskModal.addEventListener('click', function(e) {
+        if (e.target === editTaskModal) {
+            editTaskModal.style.display = 'none';
         }
     });
 
@@ -65,12 +64,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
         categorySelect.value = category;
     });
-
-    const editTaskModal = document.getElementById('edit-task-modal');
-    
-    editTaskModal.addEventListener('click', function(e) {
-        if (e.target === editTaskModal) {
-            editTaskModal.classList.add('hidden');
-        }
-    });
 });
+
+function openEditForm(taskId, title, category, dueDate) {
+    document.getElementById('edit-task-title').value = title;
+    document.getElementById('edit-task-category').value = category;
+    document.getElementById('edit-task-date').value = dueDate;
+    document.getElementById('edit-task-form').action = `/edit_task/${taskId}/`;
+    document.getElementById('edit-task-modal').style.display = 'block';
+}
